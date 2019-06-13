8555253759269e8c510bb1e29583302ffbdd87db
CountNick <meijer.nick1@gmail.com>
2019-06-13 16:55:12 +0200

    :ambulance: updating bio issue fixed

--- routes/editProfile.js
+++ routes/editProfile.js
@@ -1,18 +1,34 @@
+/* eslint-disable no-console */
 var express = require('express');
-var editProfile = express.Router();
+var router = express.Router();
 var User = require('../models/users.model');
 
-editProfile.get('/', function(req, res) {
+router.get('/', function(req, res) {
     
     User.findOne({_id: process.env.SESSION_SECRECT}, function(err, users){
 
-            res.render('editProfile', {title: 'Edit your profile', users: users});
-        
-        
-    });
-
-
-    
+            res.render('editProfile', {title: 'Edit your profile', users: users}
+            );
+        });
 });
 
-module.exports = editProfile;
+router.post('/', function(req, res){
+    console.log("a post came in with body", req.body);
+
+    User.findByIdAndUpdate(
+        {_id: process.env.SESSION_SECRECT},
+        {bio: req.body.bio}, 
+        {upsert: true}, function(err, result){
+            if(err){
+                console.log("post resulted in error", err);
+            }
+            else{
+                console.log('gelukt', result);
+                // res.redirect('/');
+            }
+        }   
+    );
+});
+
+
+module.exports = router;
