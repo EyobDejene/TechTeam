4aa52f4dc2cf03c7568d03043da9ab8ef5b83fd1
CountNick <meijer.nick1@gmail.com>
2019-06-13 10:28:51 +0200

    :building_construction: added user from database in editProfile route

--- routes/editProfile.js
+++ routes/editProfile.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var editProfile = express.Router();
+var User = require('../models/users.model');
 
 editProfile.get('/', function(req, res) {
+    
+    User.findOne({_id: process.env.SESSION_SECRECT}, function(err, users){
 
-    res.render('editProfile', {title: 'Edit your profile',});
+            res.render('editProfile', {title: 'Edit your profile', users: users});
+        
+        
+    });
+
+
+    
 });
 
 module.exports = editProfile;
