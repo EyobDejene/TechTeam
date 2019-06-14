db5cf83a3cab8a887705239315992c2ec5f03d3b
eyobdejene <eyob_westerink@hotmail.com>
2019-06-14 10:17:29 +0200

    ✨ explore people
    added new function in index route to get user data from mogodb" #4

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,55 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+    if(req.session.user) {
+        var skillLevel = req.session.skillLevel;
+        // var userAge = calculate_age(new Date(req.session.userAge));
+        var runningScheme = req.session.runningScheme;
+        var practiceTime = req.session.practiceTime;
+        var distance = req.session.userLocation;
+        var maxDistance = 35;
 
-    res.render('index', {title: 'Insync',});
+        function calculate_age(age) {
+            var date = new Date();
+            return Math.abs(age.getFullYear() - date.getFullYear());
+        }
+
+        // console.log("distance " + distance);
+        // console.log("age " + maxAge);
+
+        userModel.find({
+            skill_level: skillLevel,
+            location: {$lte: maxDistance},
+            // running_scheme: runningScheme,
+            // practice_time: practiceTime
+        }).select('first_name age avatar location match_date bio')
+            .exec()
+            .then(function (users) {
+                if (req.xhr) {
+                    res.json(users);
+                    console.log(users);
+                } else {
+                    res.render('index', {
+                        title: 'inSync',
+                        users: users,
+                        filter: false,
+                        session: req.session.user
+                    });
+
+                }
+            })
+            .catch(err => {
+                res.status(500).json({
+                    error: err
+                });
+            });
+    }else{
+        res.status(301).redirect("/login");
+        // res.render('login', {
+        //     message: 'No session has been found -> first login'
+        // });
+    }
+
 
 });
 
